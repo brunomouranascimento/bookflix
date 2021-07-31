b4473f851de3f114c8f4dbc197b37629592f91e8
Bruno Nascimento <bruno.bmn@gmail.com>
2021-07-31 17:49:58 -0300

    feat: react bootstrap

--- src/app/components/SearchBar.jsx
+++ src/app/components/SearchBar.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
+import { Container, Row, InputGroup, FormControl } from "react-bootstrap";
+
 export default function SearchBar() {
   return (
-    <div>
-      <h1>SearchBar</h1>
-    </div>
+    <Container>
+      <Row>
+        <h1 className="flex">Bookflix</h1>
+        <InputGroup className="mb-3">
+          <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+          <FormControl
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+      </Row>
+    </Container>
   );
 }
